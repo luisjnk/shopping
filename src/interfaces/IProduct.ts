8399cded0cbcc2f80bc9e37f1a0754dd8fb8cb5f
Luís Junqueira <ljunqueira@Growins-MacBook-Pro.local>
2020-10-24 15:08:25 +0100

    Add product service

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,11 @@
+export type IProduct = {
+  id: string,
+  createdAt: string,
+  productName: string,
+  price: number,
+  product: string,
+  departament: string,
+  color: string,
+  material: string,
+  adjectives: string
+}
